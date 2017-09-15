8626976bb9ee220de88997cffbd7818b24572915
Deysi <sude.90@gmail.com>
2017-09-15 16:24:10 -0500

    editando js

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+var caracteress = "XYZ";
+var longituds = 3;
+
+function codigoRamdon(caracteres, longitud) {
+    var codigo = "";
+    for (x = 0; x < longitud; x++) {
+        var randon = Math.floor(Math.random() * caracteres.length);
+        codigo += caracteres.substr(randon, 1);
+    }
+    return codigo;
+}
+
+alert("LAB-" + codigoRamdon(caracteress, longituds));
+
+
+
+
+
+/*function rand_code(chars, lon){
+code = "";
+for (x=0; x < lon; x++)
+{
+rand = Math.floor(Math.random()*chars.length);
+code += chars.substr(rand, 1);
+}
+return code;
+}
+
+caracteres = "0123456789";
+longitud = 3;
+
+alert("XYZ" +rand_code(caracteres, longitud));  
+//devuelve una cadena aleatoria de 20 caracteres*/
