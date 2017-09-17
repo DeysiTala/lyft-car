c9524391c27f2e336a8b602cc7d3f6e05168f099
carlinha talavera cardenas <sude.90@gmail.com>
2017-09-17 15:17:12 -0500

    funcion ramdon activando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,26 @@
 var caracteress = "XYZ";
 var longituds = 3;
 
+
 function codigoRamdon(caracteres, longitud) {
+
+    
     var codigo = "";
     for (x = 0; x < longitud; x++) {
         var randon = Math.floor(Math.random() * caracteres.length);
         codigo += caracteres.substr(randon, 1);
     }
     return codigo;
+
+ }
+
+
+function cod(){
+
+codigoRamdon();
+	alert("Su codigo es:" + "LAB-" + codigoRamdon(caracteress, longituds));
 }
-
-alert("LAB-" + codigoRamdon(caracteress, longituds));
-
-
+ //document.getElementById("numale").innerHTML = "LAB-" + codigoRamdon(caracteres,longituds);
 
 
 
