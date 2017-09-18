e9c5c86b19eb9f075011c7b29f7eadf9117d4b08
carlinha talavera cardenas <sude.90@gmail.com>
2017-09-17 22:53:56 -0500

    agregando el css

---
+++ assets/js/flags.js
@@ -0,0 +1,18 @@
+ $('#basic').flagStrap();
+
+$('.select-country').flagStrap({
+	
+	buttonSize: "btn-sm",
+	buttonType: "btn-info",
+	labelMargin: "10px",
+	scrollable: false,
+	scrollableHeight: "350px"
+});
+
+/*$('#advanced').flagStrap({
+	buttonSize: "btn-lg",
+	buttonType: "btn-primary",
+	labelMargin: "20px",
+	scrollable: false,
+	scrollableHeight: "350px"
+});*/
--- assets/js/main.js
+++ assets/js/main.js
@@ -26,20 +26,4 @@ codigoRamdon();
 
 
 
-/*function rand_code(chars, lon){
-code = "";
-for (x=0; x < lon; x++)
-{
-rand = Math.floor(Math.random()*chars.length);
-code += chars.substr(rand, 1);
-}
-return code;
-}
-
-caracteres = "0123456789";
-longitud = 3;
-
-alert("XYZ" +rand_code(caracteres, longitud));  
-//devuelve una cadena aleatoria de 20 caracteres*/
-
-
+ 
